d9dd180c2ff8e225c6964ac2e6463ae02198c25b
Henrik Srapyan <hensrap@gmail.com>
2024-07-23 13:15:46 +0400

    ArrayHomework

--- src/homework/ArrayHomework.java
+++ src/homework/ArrayHomework.java
@@ -3,6 +3,7 @@ package homework;
 public class ArrayHomework {
     public static void main(String[] args) {
         int[] nums = {6, 8, 4, 7, 5, 6, 1, 4};
+        System.out.println("1 վարժություն");
         int n = 6;
         int sum = 0;
 
@@ -12,6 +13,19 @@ public class ArrayHomework {
             }
         }
         System.out.println(sum);
+        System.out.println();
 
+
+        System.out.println("3 վարժություն");
+        int x;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                x = nums[j];
+                if (x == nums[i]) {
+                    sum += 1;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
