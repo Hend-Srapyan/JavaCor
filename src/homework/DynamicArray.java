186a4648d9a21db42bd26cca3fb36eec75010bbf
Henrik Srapyan <hensrap@gmail.com>
2024-08-16 16:24:14 +0400

    DynamicArray

--- src/homework/DynamicArray.java
+++ src/homework/DynamicArray.java
@@ -43,4 +43,38 @@ public class DynamicArray {
         }
     }
 
+    //ջնջել մասիվի index-երորդ էլեմենտը
+    //եթե չկա նման ինդեքսով էլեմենտ, տպել, որ չկա նման էլեմենտ
+    public void deleteByIndex(int index){
+
+    }
+
+    //տրված value-ն դնելու ենք տրված index-ով էլեմենտի տեղը։
+    //Հին արժեքը կկորի
+    //եթե նման ինդեքսով էլեմենտ չկա, գրում ենք որ չկա։
+    public void set(int index, int value){
+
+    }
+
+    //ավելացնել տրված value-ն տրված ինդեքսում, իսկ էղած էլեմենտները մի հատ աջ տանել։
+    //Եթե չկա նման ինդեքս, տպել որ չկա
+    public void add(int index, int value){
+
+    }
+
+    //Վերադարձնել true եթե տրված value-ն կա մեր մասիվի մեջ, եթե ոչ false
+    public boolean exists(int value){
+        if (value == array[size]){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    //Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+    //եթե չկա, -1
+    public int getIndexByValue(int value){
+        return 0;
+    }
+
 }
